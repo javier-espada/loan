441ec99ca28a1f047ef38c71ee65be1a498ebbd3
Carlos.Rivera <carlos.rivera@qaracter.com>
2026-01-12 10:51:12 +0100

    feature: controller structure created, LoanService createMethod modified

--- src/main/java/com/example/loan/services/LoanService.java
+++ src/main/java/com/example/loan/services/LoanService.java
@@ -4,15 +4,21 @@ import com.example.loan.models.Loan;
 import com.example.loan.models.Status;
 import com.example.loan.models.User;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 public class LoanService {
 
+    private long loanID = 0L;
+    private static HashMap<Long, Loan> loanList = new HashMap<>();
+
     public LoanService() {
     }
 
-    public Loan createLoan(double amount, int repaymentPeriod, User user) {
-        return new Loan(amount, repaymentPeriod, user);
+    public void createLoan(double amount, int repaymentPeriod, User user) {
+        loanList.put(loanID++, new Loan(amount, repaymentPeriod, user));
     }
 
     public Loan paidLoan(long idLoan, User user) {
