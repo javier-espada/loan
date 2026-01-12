6f6379a321572b33853644381599ae70a6f010ea
javier-espada <javier.espada@qaracter.com>
2026-01-12 11:16:08 +0100

    LoanController

--- src/main/java/com/example/loan/controllers/LoanController.java
+++ src/main/java/com/example/loan/controllers/LoanController.java
@@ -1,6 +1,9 @@
 package com.example.loan.controllers;
 
 import com.example.loan.models.Loan;
+import com.example.loan.models.User;
+import com.example.loan.services.LoanService;
+import com.example.loan.services.ProcesService;
 import com.example.loan.services.TestUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,13 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class LoanController {
     private TestUserService testUserService;
+    private LoanService loanService;
 
     @Autowired
-    public LoanController(TestUserService testUserService) {this.testUserService = testUserService;}
+    public LoanController(TestUserService testUserService, LoanService loanService) {this.testUserService = testUserService;}
 
     @PostMapping
-    public Loan createLoan(@RequestBody Loan loan) {
-        return loan;
+    public Loan createLoan(@RequestParam double amount, @RequestParam int repaymentPeriod, @RequestBody User user) {
+       Loan loan = loanService.createLoan(amount, repaymentPeriod, user);
+       ProcesService loanProcess = new ProcesService (user,loan);
+       loanProcess.start();
+       return loan;
     }
+
+
 
 }
